10ea7caa4800d231a247aeed8fe6b599a629f2a7
Isaiah <isaiah.adams7895@gmail.com>
2019-06-26 10:13:04 +0430

    made a nice server

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const express = require('express');
-
-const app = express();
-
-app.listen(3000)
-console.log("Server is live on port 3000")
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000, function(error){
+    if(error)
+        console.log(error);
+    console.log('Server is live on port 3000')
+})
